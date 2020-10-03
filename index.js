5a946ea818e61ed23e1218820e91fbf4b86ccd73
Eva Viss <evagraceviss@gmail.com>
2020-10-03 04:45:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
-  } else {return "I love you, too."}
+  } else { 
+    return "I love you, too."
+}
 }
 function logShout(string) {
   console.log(string.toUpperCase())
